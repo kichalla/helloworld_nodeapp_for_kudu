2b5a99819a32fd8e8e09c6b3358a2904ded0d395
Kiran Challa <kichalla@users.noreply.github.com>
2018-08-22 11:54:14 -0700

    updated

--- app.js
+++ app.js
@@ -3,7 +3,5 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-// app.listen(3000, function () {
-//   console.log('Example app listening on port 3000!');
-// });
+// Here port is supplied by Kudu 
 app.listen(process.env.PORT);
